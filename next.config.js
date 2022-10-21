6fa38017aabaa9ca6a7dc0f077053a7255af26d3
Mark Struik <mark.struik@devops.ncia.nato.int>
2022-10-21 10:43:56 +0200

    docker support + routing tests

--- next.config.js
+++ next.config.js
@@ -3,13 +3,21 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   poweredByHeader: false,
-  rewrites: async () => [{
-    source: '/:symbolcode',
-    destination: '/api/symbol/:symbolcode',
-  }, {
-    source: '/',
-    destination: '/api/symbol/SFG-UCI---'
-  }]
-}
+  output: 'standalone',
+  rewrites: async () => [
+    {
+      source: ":size/:custom/P-/:symbolcode/False/NONE",
+      destination: "/api/symbol/:symbolcode?size=:size",
+    },
+    {
+      source: "/:symbolcode",
+      destination: "/api/symbol/:symbolcode",
+    },
+    {
+      source: "/",
+      destination: "/api/symbol/SFG-UCI---",
+    },
+  ],
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
